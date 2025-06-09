0acd0712bd21f4b93f99e9ab160a0f854adeb6b2
DurgadasJ <durgadas.vijay.joshi@gmail.com>
2025-06-09 23:53:23 +0530

    Controller Alignments

--- src/main/java/com/js/java/controller/TaskController.java
+++ src/main/java/com/js/java/controller/TaskController.java
@@ -16,9 +16,7 @@ import java.util.*;
 public class TaskController {
 
     private final TaskServiceImpl taskService;
-    
     private final TaskFormatter simpleTaskFormatter;
-    
     private final TaskFormatter defaultTaskFormatter;
 
     public TaskController(TaskServiceImpl taskService, TaskFormatter simpleTaskFormatter,
@@ -26,31 +24,27 @@ public class TaskController {
         this.taskService = taskService;
         this.simpleTaskFormatter = simpleTaskFormatter;
         this.defaultTaskFormatter = defaultTaskFormatter;
-    }
 
+    }
     @PostMapping
     public ResponseEntity<String> submitTask(@RequestBody TaskRequest request){
          taskService.submitTask(request,task -> "Fomatted Task: " + task.getTaskId());
          return ResponseEntity.ok("Task submitted successfully ");
     }
-
     @PostMapping("/simple")
     public ResponseEntity<String> submitSimpleFormatter(@RequestBody TaskRequest request){
         taskService.submitTask(request,simpleTaskFormatter);
         return ResponseEntity.ok("Task submitted by Simple Formatter");
     }
-
     @PostMapping("/default")
     public ResponseEntity<String> submitDefaultFormatter(@RequestBody TaskRequest request){
         taskService.submitTask(request,defaultTaskFormatter);
         return ResponseEntity.ok("Task submitted by Default Formatter");
     }
-
     @GetMapping
     public ResponseEntity<List<Task>> getAllTask(){
         return ResponseEntity.ok(taskService.getAllTasks());
     }
-
     @GetMapping("/{id}")
     public ResponseEntity<Task> getTaskById(@PathVariable String id){
 
@@ -58,5 +52,4 @@ public class TaskController {
                 .orElseThrow(() -> new TaskNotFoundException(id));
         return ResponseEntity.ok(task);
     }
-
 }
